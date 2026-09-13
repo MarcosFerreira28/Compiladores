dc8b58629fb338b750b769771e7bf091085e5706
joao Balaciano <joaoporta@id.uff.br>
2026-09-13 20:28:05 -0300

    feat: reconhecendo palavras reservadas

--- Makefile
+++ Makefile
@@ -1,17 +1,29 @@
 CC = gcc
+FLEX = flex
 
 CFLAGS = -Wall -Wextra -Iinclude
 
 TARGET = cminus
 
-SRC = src/main.c \
-      src/lexer/token.c
 
-all:
+LEXER_SRC = src/lexer/scanner.l
+LEXER_GEN = build/lex.yy.c
+
+SRC = src/main.c \
+      src/lexer/token.c \
+      $(LEXER_GEN)
+
+all: $(TARGET)
 	$(CC) $(CFLAGS) $(SRC) -o $(TARGET)
+
+$(LEXER_GEN): $(LEXER_SRC)
+mkdir -p build
+$(FLEX) -o $(LEXER_GEN) $(LEXER_SRC)
 
 clean:
 	rm -f $(TARGET)
+	rm -rf build
+
 
 run:
-	./$(TARGET) tests/test.cm
+	./$(TARGET) tests/lexer/palavras_reservadas.cm
--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+
+
+int yylex(void);
+
 
 int main(int argc, char *argv[])
 {
@@ -8,8 +14,20 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Compilador C-\n");
-    printf("Arquivo de entrada: %s\n", argv[1]);
+    yyin = fopen(argv[1], "r");
+
+    if (yyin == NULL){
+        printf("Erro ao abrir o arquivo: %s\n", argv[1]);
+        return 1;
+    }
+    int token;
+    do {
+        token = yylex();
+
+        printf("%-15s lexema: %-10s linha: %d\n", token_type_to_string(token), yytext, yylineno);
+    } while (token != TOKEN_EOF);
+
+    fclose(yyin);
 
     return 0;
 }
